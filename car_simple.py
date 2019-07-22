cab6df6bb6e426af834b82613157955f8888f23a
Guglielmo Manneschi <guglielmo.manneschi@mail.polimi.it>
2019-07-22 11:00:26 +0200

    car_simple

--- car_simple.py
+++ car_simple.py
@@ -6,46 +6,70 @@ from util.learner_nn import *
 np.set_printoptions(precision=6)
 np.set_printoptions(suppress=True)
 
-mdp = car_conf.ConfDrivingEnv(model_w = 100, renderFlag=True)
-mdp.horizon = 100
-
 policy = nnGaussianPolicy(nStateFeatures=4,actionDim=2,nHiddenNeurons=4,paramInitMaxVal=0.01,variance=0.1)
-learner = nnGpomdpLearner(mdp,policy,gamma=0.996)
 
-params = np.load("car_params_0101.npy")
+type1err_tot = 0
+type2err_tot = 0
 
-sfMask = np.array([0,1,0,1],dtype=np.bool)
+NUM_EXPERIMENTS = 5
 
-learn(
-	learner = learner,
-	steps = 0, # 100 steps with 25 episodes is ok
-	initParams=params,
-	nEpisodes = 25,
-	sfmask=sfMask,
-	learningRate = 0.01,
-	plotGradient = False,
-	printInfo = True
-)
+for experiment in range(NUM_EXPERIMENTS):
+	mdp = car_conf.ConfDrivingEnv(model_w = -200+400*np.random.rand(), renderFlag=False)
+	mdp.horizon = 100
 
-#np.save("car_params_0101.npy",policy.get_params())
+	learner = nnGpomdpLearner(mdp,policy,gamma=0.996)
 
-N = 500
+	#params = np.load("car_params_0101.npy")
 
-eps = collect_car_episodes(mdp,policy,N,mdp.horizon,sfmask=None,render=False,showProgress=True)
+	#sfMask = np.array([0,1,0,1],dtype=np.bool)
+	sfMask = np.random.choice(a=[False, True], size=(4), p=[0.5, 0.5])
+	while np.count_nonzero(sfMask)!=1 and np.count_nonzero(sfMask)!=2:
+		sfMask = np.random.choice(a=[False, True], size=(4), p=[0.5, 0.5])
+	print("experiment ",experiment,"- sfMask =",sfMask)
 
-sfTestMask = np.array([0,0,0,0],dtype=np.bool)
-lr_lambda = lrTest(
-	eps=eps,
-	policyInstance=policy,
-	sfMask=sfTestMask,
-	nsf=4,
-	na=2,
-	lr=0.003,
-	batchSize=N,
-	epsilon=0.001,
-	maxSteps=25000,
-	numResets=1
-)
+	learn(
+		learner = learner,
+		steps = 100, # 100 steps with 25 episodes is ok
+		#initParams=params,
+		nEpisodes = 25,
+		sfmask=sfMask,
+		learningRate = 0.01,
+		plotGradient = False,
+		printInfo = True
+	)
 
-print("REAL AGENT MASK",sfMask)
-print("ESTIMATED AGENT MASK",sfTestMask)
+	#np.save("car_params_0101.npy",policy.get_params())
+
+	N = 100
+
+	eps = collect_car_episodes(mdp,policy,N,mdp.horizon,sfmask=None,render=False,showProgress=True)
+
+	sfTestMask = np.array([0,0,0,0],dtype=np.bool)
+	lr_lambda = lrTest(
+		eps=eps,
+		policyInstance=policy,
+		sfMask=sfTestMask,
+		nsf=4,
+		na=2,
+		lr=0.003,
+		batchSize=N,
+		epsilon=0.001,
+		maxSteps=25000,
+		numResets=1
+	)
+
+	print("REAL AGENT MASK",sfMask)
+	print("ESTIMATED AGENT MASK",sfTestMask)
+
+	x = np.array(sfTestMask,dtype=np.int32)-np.array(sfMask,dtype=np.int32)
+	type1err = np.count_nonzero(x == 1) # Rejected features the agent doesn't have
+	type2err = np.count_nonzero(x == -1) # Not rejected features the agent has
+	print("Type 1 error =",type1err/4.0)
+	print("Type 2 error =",type2err/4.0)
+	type1err_tot += type1err
+	type2err_tot += type2err
+	print("Type 1 error frequency =",np.float32(type1err_tot)/4.0/np.float32(experiment+1))
+	print("Type 2 error frequency =",np.float32(type2err_tot)/4.0/np.float32(experiment+1))
+
+print("[OUTPUT] Type 1 error frequency =",np.float32(type1err_tot)/4.0/np.float32(NUM_EXPERIMENTS))
+print("[OUTPUT] Type 2 error frequency =",np.float32(type2err_tot)/4.0/np.float32(NUM_EXPERIMENTS))
